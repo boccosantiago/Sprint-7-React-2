7440fc94ea840d19999ce343191ed65f76bf2b28
BoccoSantiago <italobocco@gmail.com>
2022-07-04 19:18:14 +0200

    Ejercicio 1

--- src/App.js
+++ src/App.js
@@ -4,43 +4,64 @@ import { useState } from 'react';
 
 function App() {
 
-const [price, setPrice] = useState(0)
-  
-function calculateTotal (){
-  
-} 
-/* function handleChangeWeb(event){
-    const webPage = event.target.checked
-    console.log(webPage)
-    if (webPage === true){ setPrice(500)
-    } else {setPrice(prevPrice => prevPrice)}
+
+const [data, setData] = useState({
+  webPage: false,
+  seo: false,
+  ads: false
+})
+
+function handleChange(event){
+  const {name, checked} = event.target 
+
+  setData(prevData => {
+    return {
+      ...prevData,
+      [name]: checked
     }
-function handleChangeSeo(event){
-      const webPage = event.target.checked
-      console.log(webPage)
-      if (webPage === true){ setPrice(300)
-      } else {setPrice(prevPrice => prevPrice)}
+  })
+}
 
-    }
-function handleChangeAds(event){
-    const webPage = event.target.checked
-        console.log(webPage)
-     if (webPage === true){ setPrice(200)
-     } else {setPrice(prevPrice => prevPrice)}
-    } */
+function totalSum(){
+  let total = (data.webPage ? 500 : 0) +
+  (data.seo ? 300 : 0) +
+  (data.ads ? 200 : 0)
 
-
+  return total
+}
 
   return (
     <div>
       <h3>¿Que quieres hacer?</h3>
-      <input type="checkbox" value={500} onChange={handleChangeWeb}/><label>Una pagina web (500€)</label><br/>
-      <input type="checkbox" value={300} onChange={handleChangeSeo}/><label>Una consultoria SEO (300€)</label><br/>
-      <input type="checkbox" value={200}  onChange={handleChangeAds}/><label>Una campaña de Google Ads (200€)</label>
-      <h3>Precio: {price} € </h3>
+      <input 
+      type="checkbox" 
+      name= "webPage"
+      id="webPage"
+      onChange={handleChange}
+      checked={data.webPage}
+      />
+      <label htmlFor="webPage">Una pagina web (500€)</label>
+      <br/>
+      <input 
+      type="checkbox" 
+      name= "seo"
+      id="seo"
+      onChange={handleChange}
+      checked={data.seo}
+      />
+      <label htmlFor="seo">Una consultoria SEO (300€)</label>
+      <br/>
+      <input 
+      type="checkbox" 
+      name= "ads"
+      id="ads"
+      onChange={handleChange}
+      checked={data.ads}
+      />
+      <label htmlFor="ads">Una campaña de Google Ads (200€)</label>
+      <h3>Precio: {totalSum()}  € </h3>
     </div>
   );
 }
-
 
 export default App;
