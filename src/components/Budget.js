55d03afbbc6c54821ebb026ef096758e1676f531
BoccoSantiago <italobocco@gmail.com>
2022-07-12 13:04:09 +0200

    Ejercicio 8

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,47 +4,76 @@ import "./Budget.css";
 export default function Budget(props) {
   const [users, setUsers] = useState([]);
 
-  console.log(users)
+  console.log(users);
+
+  const current = new Date();
+
+  const date = current.toString().slice(0,24)
+
+ /*  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()} ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`; */
 
   function handleUsers() {
- 
-    
     const newUser = {
+      budget: props.budget,
+      client: props.client,
+      date: date,
+      currentDate: current,
+      total: props.total,
       webPage: props.webPage,
       seo: props.seo,
       ads: props.ads,
       numPage: props.numPage,
       numLang: props.numLang,
-      budget: props.budget,
-      client: props.client,
-      total: props.total,
     };
 
-    setUsers(prevUsers => [...prevUsers, newUser]);
+    setUsers((prevUsers) => [...prevUsers, newUser]);
   }
 
-  const current = new Date();
-  const date = `${current.getDate()}/${
-    current.getMonth() + 1
-  }/${current.getFullYear()}`;
+  function alphabetical() {
+    let result = users.sort((a, b) => {
+      return a.budget > b.budget ? 1 : -1;
+    });
+    setUsers([...result]);
+  }
 
+  function orderByDate() {
+    let result = users.sort((a, b) => {
+      return b.currentDate - a.currentDate 
+    });
+    setUsers([...result]);
+  }
+
+  function defaultOrder() {
+    let result = users.sort((a, b) => {
+      return a.currentDate - b.currentDate
+    });
+    setUsers([...result]);
+  }
 
   const budgetForm = users.map((user, index) => {
     return (
-    <div className="budget" key={index}>
-    <p>Fecha: {date} </p>
-    <p>Nombre del presupuesto: <br/> {user.budget} </p>
-    <p>Nombre de cliente: <br/> {user.client}</p>
-    <p>Precio: {user.total} € </p>
-    </div>)})
-  
+      <div className="budget" key={index}>
+        <p>{user.date}</p>
+        <p>Nombre del presupuesto: <br /><p className="text">{user.budget}</p> </p>
+        <p> Nombre de cliente: <br /><p className="text"> {user.client}</p> </p>
+        <p>Precio: {user.total} € </p>
+      </div>
+    );
+  });
 
   return (
     <div className="main-container">
-      <button onClick={() => handleUsers()}>Agregar</button>
-      <div className="budget-container">
-      {budgetForm} 
+      <div className="buttons-order">
+        <button onClick={alphabetical}>Ordenar Alfabéticamente</button>
+        <button onClick={orderByDate}>Más recientes</button>
+        <button onClick={defaultOrder}>Más antiguos</button>
       </div>
+      <button className="agregar" onClick={() => handleUsers()}>
+        Agregar
+      </button>
+      <div className="budget-container">{budgetForm}</div>
     </div>
   );
 }
